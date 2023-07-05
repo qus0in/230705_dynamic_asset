9f247ccad9afc5eb77edf16850adf654f24d44bf
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-05 20:00:51 +0900

    end_date

--- common.py
+++ common.py
@@ -2,11 +2,11 @@ from dateutil.relativedelta import relativedelta
 import yfinance as yf
 import streamlit as st
 
-def month_before(date, months):
-    return date - relativedelta(months=months)
+def month_before(base_date, months):
+    return base_date - relativedelta(months=months)
 
 @st.cache_data
 def get_history(ticker, start_date, end_date):
-    history = yf.Ticker(ticker).history(start=base_date_mo12, end=base_date)
+    history = yf.Ticker(ticker).history(start=start_date, end=end_date)
     history.index = history.index.date
     return history
