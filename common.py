056945fbbb4528ab5142522cc8442043ad612ec5
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-05 20:04:42 +0900

    st.session_state.base_date

--- common.py
+++ common.py
@@ -2,8 +2,8 @@ from dateutil.relativedelta import relativedelta
 import yfinance as yf
 import streamlit as st
 
-def month_before(base_date, months):
-    return base_date - relativedelta(months=months)
+def month_before(months):
+    return st.session_state.base_date - relativedelta(months=months)
 
 @st.cache_data
 def get_history(ticker, start_date, end_date):
